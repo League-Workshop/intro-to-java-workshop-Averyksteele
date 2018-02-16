45fb9b904d303e91178b033e96562c5583f1a4b9
Averyksteele <league@iMac.attlocal.net>
2018-02-16 15:01:42 -0800

    Day 1 workshop

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -4,12 +4,24 @@ import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	
+
 	public static void main(String[] args) {
-		
-		// START HERE
-	
-		
-		
+
+		Robot Robby = new Robot();
+		Robby.penDown();
+		for (int i = 0; i < 100; i++) {
+			Robby.setSpeed(100);
+		Robby.setPenColor(Color.PINK);
+		Robby.move(100);
+		Robby.turn(80);
+		Robby.move(100);
+		Robby.turn(70);
+		Robby.move(200);
+		}
+	}
+
+	private static void setSpeed(int i) {
+		// TODO Auto-generated method stub
+
 	}
 }
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -1,25 +1,30 @@
 package section2;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
-		// 5. Set your robot's pen down 
-		
+		Robot Rob = new Robot();
+		// 5. Set your robot's pen down
+		Rob.penDown();
 		// 3. Set the robot to go at max speed (10)
-		
+		Rob.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+		for (int i = 0; i < 100; i++) {
 			// 7. Change the pen color to random
-		
+
+			Rob.setPenColor(Color.MAGENTA);
+
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
-			
+			Rob.move(5 * i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+			Rob.turn(360 / 7);
 			// 8. Set the pen width to i
-			
+			Rob.setPenWidth(i);
+		}
 	}
 }
